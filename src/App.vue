968a2e322141d3433a3a2e588cecba1fcbf6c35c
kyoungstar <fatstar@hanmail.net>
2023-12-16 12:26:46 +0900

    header 임시

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="App">
+    <NeoHeader></NeoHeader>
     <MainHome></MainHome>
   </div>
 </template>
 
 <script>
 import MainHome from './components/MainHome.vue'
+import NeoHeader from './components/NeoHeader.vue'
+
 export default {
   components: {
+    'NeoHeader' : NeoHeader,
     'MainHome' : MainHome
   }
 }
 </script>
 
-<style lang="scss" scoped>
-@import "/src/assets/scss/main.scss";
+<style lang="scss">
+ @import "/src/assets/scss/common.scss";
 </style>
---
+++ src/components/NeoHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="_3UiAr">
+        <div class="_2RM-E">
+            <div class="_10wKD">
+                <div class="_5sz4t">
+                    <div class="_3H--o">
+                        <a name="visit-home-page-1" class="_3TXr5" data-test-hint="SearchBox-Link-1" href="/"><img src="https://assets.talkshop.live/images/tsl-logo-talkshoplive_r.svg" alt="talkshoplive" height="30" width="158"></a>
+                        <div class="_3pGxH">
+                            <i class="far fa-search _3t6Z3" aria-hidden="true">
+                            </i>                       
+                            <svg height="16" width="16" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                            <input type="search" class="_1Kn4I" placeholder="Search" data-test-hint="SearchBox-input-1" value="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a name="sign-in" class="btn TMMgS" data-role="login-button" href="/login?next=/features/creators">Sign In</a>
+        <div class="xPSLv">
+            <div class="HCJQe">
+                <div class="_2alre">
+                    <div class="_2qJUS" data-test-hint="DropDownUserMenu-div-1">
+                        <div class="_3-_ci"><div class="_3OqyK">
+                            <i class="far fa-bars B3L0S" aria-hidden="true"></i>
+                            <svg height="16" width="14" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button aria-label="Open cart" name="open-cart" class="_268AS" data-abbey="VIEW_CART_SIDEBAR'" data-test-hint="MainNav-button-3"><img alt="Cart icon" src="/public/images/tsl-logo-color.svg" height="16" width="24" data-abbey="VIEW_CART_SIDEBAR'"></button>
+        </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+   
+}
+</script>
+
+
+<style lang="scss">
+  @import "/src/assets/scss/header.scss";
+</style>
