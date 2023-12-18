1d02d2a458013cab091dd7a1aa7094ba748ce6b0
kyoungstar <fatstar@hanmail.net>
2023-12-18 23:21:04 +0900

    slider vue3 최종 적용

--- src/components/MainSlider.vue
+++ src/components/MainSlider.vue
@@ -2,198 +2,93 @@
     <div class="_14kIw vOVaW">
         <h2>New &amp; upcoming</h2>
         <div class="-HF5e _3ZKs_">
-            <div class="alice-carousel">
-                <div>
-                    <div class="alice-carousel__wrapper">
-                        <ul class="alice-carousel__stage">
-                            <li class="alice-carousel__stage-item">
-                                <div class="_3HFcX">
-                                    <div class="V3D_d">
-                                        <a class="wHpww" data-test-hint="UniversalShowCard-Link-1" href="/watch/vcVds3INGO-z">
-                                            <div class="_114Vk">
-                                                <img src="https://cdn.talkshop.live/eyJidWNrZXQiOiJ0c2wtaW1hZ2VzLXByb2R1Y3Rpb24iLCJrZXkiOiJ2YXJpYW50X2ltYWdlcy83MzYxODA4L29yaWdpbmFsL2Vhc3RvbmNvcmJpbi1saXZlLWZyb20tY293Ym95LWNocmlzdG1hcy5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjYwMCwiZml0IjoiY29udGFpbiJ9fX0=" data-test-hint="UniversalShowCard-img-1">
+            
+            <div class="vue3-carousel__stage">
+                <Carousel v-bind="settings" :breakpoints="breakpoints" :wrap-around="false">
+                    <Slide v-for="item in items" :key="item">
+                    
+                        <div class="vue3-carousel__stage-item">
+                            <div class="_3HFcX">
+                                <div class="V3D_d">
+                                    <a class="wHpww" data-test-hint="UniversalShowCard-Link-1" href="/watch/vcVds3INGO-z">
+                                        <div class="_114Vk">
+                                            <img :src="item.img"/>
+                                        </div>
+                                    </a>
+                                    <div class="_2hEzG">
+                                        <a class="_1oz5H" data-test-hint="UniversalShowCard-Link-2" href="/channels/channel3">
+                                            <div class="_1TW9y _1U0Sp">
+                                                <img alt="Avatar image" class="_2qclA _3gH7F" :src="item.avt"/>
                                             </div>
                                         </a>
-                                        <div class="_2hEzG">
-                                            <a class="_1oz5H" data-test-hint="UniversalShowCard-Link-2" href="/channels/channel3">
-                                                <div class="_1TW9y _1U0Sp">
-                                                    <img alt="Avatar image" class="_2qclA _3gH7F" src="https://cdn.talkshop.live/eyJidWNrZXQiOiJ0c2wtaW1hZ2VzLXByb2R1Y3Rpb24iLCJrZXkiOiJ2YXJpYW50X2ltYWdlcy83MzYwNDIxL29yaWdpbmFsL2Vhc3RvbmNvcmJpbi5zdG9yZV9hdmF0YXIiLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjYwMCwiZml0IjoiY29udGFpbiJ9fX0=" data-test-hint="Avatar-img-1">
+                                        <div class="_2YrAy">
+                                            <div class="_1b5Q9 _1YWAI">
+                                                <div class="_1x-tW">
+                                                    <b>{{item.date}}</b> at {{item.time}}
                                                 </div>
-                                            </a>
-                                            <div class="_2YrAy">
-                                                <div class="_1b5Q9 _1YWAI">
-                                                    <div class="_1x-tW">
-                                                        <b>Nov 16th, 2024</b> at 3:34am
-                                                    </div>
-                                                    <div>
-                                                        <a class="_2jfOQ _2QGNk LkySA" data-test-hint="UniversalFollowButton-Link-1">
-                                                            <i class="fa-solid fa-bell _35vsF" aria-hidden="true"></i>Notify me
-                                                        </a>
-                                                    </div>
+                                                <div>
+                                                    <a class="_2jfOQ _2QGNk LkySA" data-test-hint="UniversalFollowButton-Link-1">
+                                                        <i class="fa-solid fa-bell _35vsF" aria-hidden="true"></i>Notify me
+                                                    </a>
                                                 </div>
-                                                <a class="_12Af-" data-test-hint="UniversalShowCard-Link-3" href="/watch/vcVds3INGO-z">Happy Hour IG Live </a>
-                                                <a class="_1nlN6 standaloneTextButton" data-test-hint="UniversalShowCard-Link-4" href="/channels/channel3">Channel3</a>
                                             </div>
+                                            <a class="_12Af-" data-test-hint="UniversalShowCard-Link-3" href="/watch/vcVds3INGO-z">{{item.title}}</a>
+                                            <a class="_1nlN6 standaloneTextButton" data-test-hint="UniversalShowCard-Link-4" href="/channels/channel3">{{item.link}}</a>
                                         </div>
                                     </div>
                                 </div>
-                            </li>
-                            
-                            <li class="alice-carousel__stage-item">
-                                <div class="_3HFcX">
-                                    <div class="V3D_d">
-                                        <a class="wHpww" data-test-hint="UniversalShowCard-Link-1" href="/watch/vcVds3INGO-z">
-                                            <div class="_114Vk">
-                                                <img src="https://cdn.talkshop.live/eyJidWNrZXQiOiJ0c2wtaW1hZ2VzLXByb2R1Y3Rpb24iLCJrZXkiOiJ2YXJpYW50X2ltYWdlcy83MzY1MjAxL29yaWdpbmFsL3RoZWJlYXV0eWVkaXQtY2FpdC1zLWhvbGlkYXktYmVhdXR5LWdpZnRpbmcuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo2MDAsImZpdCI6ImNvbnRhaW4ifX19" data-test-hint="UniversalShowCard-img-1">
-                                            </div>
-                                        </a>
-                                        <div class="_2hEzG">
-                                            <a class="_1oz5H" data-test-hint="UniversalShowCard-Link-2" href="/channels/channel3">
-                                                <div class="_1TW9y _1U0Sp">
-                                                    <img alt="Avatar image" class="_2qclA _3gH7F" src="https://cdn.talkshop.live/eyJidWNrZXQiOiJ0c2wtaW1hZ2VzLXByb2R1Y3Rpb24iLCJrZXkiOiJ2YXJpYW50X2ltYWdlcy80MjYyMDQvb3JpZ2luYWwvdGhlYmVhdXR5ZWRpdC5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjYwMCwiZml0IjoiY29udGFpbiJ9fX0=" data-test-hint="Avatar-img-1">
-                                                </div>
-                                            </a>
-                                            <div class="_2YrAy">
-                                                <div class="_1b5Q9 _1YWAI">
-                                                    <div class="_1x-tW">
-                                                        <b>Nov 16th, 2024</b> at 3:34am
-                                                    </div>
-                                                    <div>
-                                                        <a class="_2jfOQ _2QGNk LkySA" data-test-hint="UniversalFollowButton-Link-1">
-                                                            <i class="fa-solid fa-bell _35vsF" aria-hidden="true"></i>Notify me
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                                <a class="_12Af-" data-test-hint="UniversalShowCard-Link-3" href="/watch/vcVds3INGO-z">Happy Hour IG Live </a>
-                                                <a class="_1nlN6 standaloneTextButton" data-test-hint="UniversalShowCard-Link-4" href="/channels/channel3">Channel3</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="alice-carousel__stage-item">
-                                <div class="_3HFcX">
-                                    <div class="V3D_d">
-                                        <a class="wHpww" data-test-hint="UniversalShowCard-Link-1" href="/watch/vcVds3INGO-z">
-                                            <div class="_114Vk">
-                                                <img src="https://cdn.talkshop.live/eyJidWNrZXQiOiJ0c2wtaW1hZ2VzLXByb2R1Y3Rpb24iLCJrZXkiOiJ2YXJpYW50X2ltYWdlcy83MzY1NTc5L29yaWdpbmFsL3JvYmVydGRvd25leWpyLWNvb2wtZm9vZC1yYW5kb21seS1zaWduZWQuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo2MDAsImZpdCI6ImNvbnRhaW4ifX19" data-test-hint="UniversalShowCard-img-1">
-                                            </div>
-                                        </a>
-                                        <div class="_2hEzG">
-                                            <a class="_1oz5H" data-test-hint="UniversalShowCard-Link-2" href="/channels/channel3">
-                                                <div class="_1TW9y _1U0Sp">
-                                                    <img alt="Avatar image" class="_2qclA _3gH7F" src="https://cdn.talkshop.live/eyJidWNrZXQiOiJ0c2wtaW1hZ2VzLXByb2R1Y3Rpb24iLCJrZXkiOiJ2YXJpYW50X2ltYWdlcy83MzU3ODM5L29yaWdpbmFsL3JvYmVydGRvd25leWpyLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjYwMCwiZml0IjoiY29udGFpbiJ9fX0=" data-test-hint="Avatar-img-1">
-                                                </div>
-                                            </a>
-                                            <div class="_2YrAy">
-                                                <div class="_1b5Q9 _1YWAI">
-                                                    <div class="_1x-tW">
-                                                        <b>Nov 16th, 2024</b> at 3:34am
-                                                    </div>
-                                                    <div>
-                                                        <a class="_2jfOQ _2QGNk LkySA" data-test-hint="UniversalFollowButton-Link-1">
-                                                            <i class="fa-solid fa-bell _35vsF" aria-hidden="true"></i>Notify me
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                                <a class="_12Af-" data-test-hint="UniversalShowCard-Link-3" href="/watch/vcVds3INGO-z">Happy Hour IG Live </a>
-                                                <a class="_1nlN6 standaloneTextButton" data-test-hint="UniversalShowCard-Link-4" href="/channels/channel3">Channel3</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="alice-carousel__prev-btn">
-                    <div class="alice-carousel__prev-btn-wrapper">
-                        <p class="alice-carousel__prev-btn-item" data-test-hint="PrevNextButton-p-1"></p>
-                    </div>
-                </div>
-                <div class="alice-carousel__next-btn">
-                    <div class="alice-carousel__next-btn-wrapper">
-                        <p class="alice-carousel__next-btn-item" data-test-hint="PrevNextButton-p-1"></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    
-    <vue-horizontal-list :items="items" :options="options">
-        <template v-slot:default="{item}">
-            <div class="alice-carousel__stage-item">
-                <div class="_3HFcX">
-                    <div class="V3D_d">
-                        <a class="wHpww" data-test-hint="UniversalShowCard-Link-1" href="/watch/vcVds3INGO-z">
-                            <div class="_114Vk">
-                                <img :src="item.img"/>
-                            </div>
-                        </a>
-                        <div class="_2hEzG">
-                            <a class="_1oz5H" data-test-hint="UniversalShowCard-Link-2" href="/channels/channel3">
-                                <div class="_1TW9y _1U0Sp">
-                                    <img alt="Avatar image" class="_2qclA _3gH7F" :src="item.avt"/>
-                                </div>
-                            </a>
-                            <div class="_2YrAy">
-                                <div class="_1b5Q9 _1YWAI">
-                                    <div class="_1x-tW">
-                                        <b>{{item.date}}</b> at {{item.time}}
-                                    </div>
-                                    <div>
-                                        <a class="_2jfOQ _2QGNk LkySA" data-test-hint="UniversalFollowButton-Link-1">
-                                            <i class="fa-solid fa-bell _35vsF" aria-hidden="true"></i>Notify me
-                                        </a>
-                                    </div>
-                                </div>
-                                <a class="_12Af-" data-test-hint="UniversalShowCard-Link-3" href="/watch/vcVds3INGO-z">{{item.title}}</a>
-                                <a class="_1nlN6 standaloneTextButton" data-test-hint="UniversalShowCard-Link-4" href="/channels/channel3">{{item.link}}</a>
                             </div>
                         </div>
-                    </div>
-                </div>
+
+                    </Slide>
+
+                    <template #addons>
+                        <Navigation />
+                    </template>
+                </Carousel>
             </div>
-        </template>
-      </vue-horizontal-list>
-    
+
+        </div>
+    </div>
 </template>
 
 <script>
-import VueHorizontalList from "vue-horizontal-list/src/vue-horizontal-list.vue";
+
+import { Carousel, Navigation, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
 
 export default {
-    name: 'ServeDev',
+    name: 'BreakPoints',
     components: {
-      VueHorizontalList
+        Carousel,
+        Slide,
+        Navigation,
     },
     data() {
       return {
-        options: {
-          responsive: [
-            {end: 1000, size: 1.2}, 
-            {start: 1000, end: 1600, size: 3.2},
-            {size: 5.2}
-          ],
-          list: {
-            // 1200 because @media (min-width: 1200px) and therefore I want to switch to windowed mode
-            windowed: 1200,
-            
-            // Because: #app {padding: 80px 24px;}
-            padding: 24
-          },
-          navigation: {
-            start: 1
-          }
-        },  
+        settings: {
+            itemsToShow: 1,
+            snapAlign: 'start',
+        },
+        breakpoints: {
+            // 1000px and up
+            1000: {
+                itemsToShow: 3,
+                snapAlign: 'start',
+            },
+            // 1600px and up
+            1600: {
+                itemsToShow: 5,
+                snapAlign: 'start',
+            }
+        },
         items: [
           {img: require('../assets/images/slider_t1.jpg'), title: 'Happy Hour IG Live', link: 'Channel3', avt: require('../assets/images/slider_t1_avt.png'), date: 'Nov 16th, 2024', time: '3:34am'},
           {img: require('../assets/images/slider_t2.jpg'), title: 'New Year, New Look!', link: 'smartcookies', avt: require('../assets/images/slider_t2_avt.jpg'), date: 'Nov 16th, 2024', time: '3:34am'},
           {img: require('../assets/images/slider_t3.jpg'), title: 'Holiday Goodies', link: 'Mike "The Situation"', avt: require('../assets/images/slider_t3_avt.jpg'), date: 'Nov 16th, 2024', time: '3:34am'},
           {img: require('../assets/images/slider_t4.jpg'), title: '2023 Luxury Review Box!', link: 'NewBeauty', avt: require('../assets/images/slider_t4_avt.jpg'), date: 'Nov 16th, 2024', time: '3:34am'},
           {img: require('../assets/images/slider_t5.jpg'), title: 'Smile brighter and whiter!', link: 'linhartnyc', avt: require('../assets/images/slider_t5_avt.png'), date: 'Nov 16th, 2024', time: '3:34am'},
-          {img: require('../assets/images/slider_t3.jpg'), title: 'Holiday Goodies', link: 'Mike "The Situation"', avt: require('../assets/images/slider_t4_avt.jpg'), date: 'Nov 16th, 2024', time: '3:34am'},
+          {img: require('../assets/images/slider_t6.jpg'), title: 'Holiday Goodies', link: 'Mike "The Situation"', avt: require('../assets/images/slider_t2_avt.jpg'), date: 'Nov 16th, 2024', time: '3:34am'},
         ]
       }
     }
