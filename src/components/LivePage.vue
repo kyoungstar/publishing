f9be583462a73fc83eecebeb7a9d936ed75353d4
kyoungstar <fatstar@hanmail.net>
2024-01-15 06:53:25 +0900

    live Slide 및 버튼 외...

--- src/components/LivePage.vue
+++ src/components/LivePage.vue
@@ -7,16 +7,95 @@
                     <div class="_2dAUU" style="height: calc(100vh - 50px); max-height: calc(100vh - 50px);">
                         <div class="_6gcP">
                             <div id="eQf-OF9sCnOT" class="_3Fj_T">
-                                <div id="parent-jwplayer" class="U0Wbi _1199X" style="background-image: url(&quot;https://cdn.talkshop.live/eyJidWNrZXQiOiJ0c2wtaW1hZ2VzLXByb2R1Y3Rpb24iLCJrZXkiOiJ2YXJpYW50X2ltYWdlcy83MzY2NTU3L29yaWdpbmFsL3ZhbGVyaWViZXJ0aW5lbGxpLWluZHVsZ2Utc2lnbmVkLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjIxMzMsImZpdCI6ImNvbnRhaW4ifX19&quot;); height: auto;">
+                                <div id="parent-jwplayer" class="U0Wbi _1199X">
                                     <div id="player-eQf-OF9sCnOT" class="jwplayer jw-reset jw-state-paused jw-stretch-uniform jw-flag-aspect-mode jw-breakpoint-5 jw-floating-dismissible zFSjj jw-flag-autostart jw-flag-user-inactive" tabindex="0" aria-label="Video Player" role="application" style="max-height: calc(100vh - 50px);">
-                                        <button type="button" style="color:#fff;background-color: #000;" v-on:click="addSlider()" v-bind:class="{hideControl:hideControl == 'true'}">열렸으면함</button>
-                                        <video class="_3EKmG" autoplay="" playsinline="" loop="" id="homepage-sizzle-player" controls="">
-                                            <!--<source src="https://assets.talkshop.live/uploads/sizzle.mp4"/>-->
-                                        </video>
+                                        
+                                            <video class="_3EKmG" autoplay="" playsinline="" loop="" id="homepage-sizzle-player" controls="" style="width:100%;height:100%;">
+                                                <!--<source src="https://assets.talkshop.live/uploads/sizzle.mp4"/>-->
+                                            </video>
+
                                     </div>
-                                    <div class="_2mTrh"></div>
-                                    <div id="streamViewHeader" class="_2jscc">....</div>
-                                    <div id="streamViewFooter" class="wNT4g">...</div>
+                                    
+                                    <div id="videoUserButtons" v-bind:class="{hideControl:hideControl == 'true'}" class="_2a6Cl" style="margin-top: 20px;">
+                                        <div class="ZIu9C">
+                                            <button name="open-chat-2" data-tip="true" data-for="tooltip_chat" class="_10GyM" data-abbey="SELECT_VIEW_CHAT_SIDEBAR" data-test-hint="StreamView-button-13" currentitem="false">
+                                                <i class="fa fa-comment" data-abbey="SELECT_VIEW_CHAT_SIDEBAR" aria-hidden="true"></i>
+                                            </button>
+                                            <div class="__react_component_tooltip place-left type-dark " id="tooltip_chat" data-id="tooltip" style="left: 735px; top: 338px;">
+                                                Live Chat
+                                            </div>
+                                        </div>
+                                        <button v-on:click="addSlider()" data-tip="true" data-for="tooltip_details" name="open-details-2" class="_3JjvR" data-abbey="SELECT_VIEW_DETAILS_SIDEBAR" data-test-hint="StreamView-button-14" currentitem="false">
+                                            <i class="fa fa-info" data-abbey="SELECT_VIEW_DETAILS_SIDEBAR" aria-hidden="true"></i>
+                                        </button>
+                                        <div class="__react_component_tooltip place-left type-dark " id="tooltip_details" data-id="tooltip" style="left: 749px; top: 386px;">
+                                            Details
+                                        </div>
+                                        <span>
+                                            <button data-tip="true" data-for="tooltip_buy" name="open-purchase-options-2" class="_2yMZh" data-abbey="VIEW_ALL_PRODUCTS" data-test-hint="StreamView-button-16" currentitem="false">
+                                                <i class="fa fa-dollar" data-abbey="VIEW_ALL_PRODUCTS" aria-hidden="true"></i>
+                                            </button>
+                                            <div class="__react_component_tooltip place-left type-dark " id="tooltip_buy" data-id="tooltip" style="left: 768px; top: 434px;">
+                                                Buy
+                                            </div>
+                                        </span>
+                                        <button name="open-share-2" data-tip="true" data-for="share" class="_1-TrG" data-abbey="SELECT_SHARE_SIDEBAR" data-test-hint="StreamView-button-17" currentitem="false">
+                                        </button>
+                                        <div class="__react_component_tooltip place-left type-dark " id="share" data-id="tooltip" style="left: 756px; top: 482px;">
+                                            Share
+                                        </div>
+                                    </div>
+                                    <div id="streamViewHeader" class="_2jscc">
+                                        <div class="_2DM94">
+                                            <div class="_1Sh63 _13eE3">
+                                                <a name="visit-channel-1" class="_3KLE- _13eE3" data-abbey="SELECT_VIEW_CHANNEL_HEADER" data-test-hint="StreamView-Link-1" href="/channels/beautyspacenk">
+                                                    <div class="_1TW9y _1U0Sp" data-abbey="SELECT_VIEW_CHANNEL_HEADER">
+                                                        <img alt="Avatar image" class="_2qclA YatIe" src="https://cdn.talkshop.live/eyJidWNrZXQiOiJ0c2wtaW1hZ2VzLXByb2R1Y3Rpb24iLCJrZXkiOiJ2YXJpYW50X2ltYWdlcy83MzUzMzYwL29yaWdpbmFsL2JlYXV0eXNwYWNlbmsucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoxMDAsImZpdCI6ImNvbnRhaW4ifX19" data-abbey="SELECT_VIEW_CHANNEL_HEADER" data-test-hint="Avatar-img-1">
+                                                    </div>
+                                                </a>
+                                                <div class="_22yBj _13eE3">
+                                                    <div class="Wh497 _13eE3">
+                                                        <div class="_1e3VX">
+                                                            <span class="_7kCzj">In partnership with</span>
+                                                            <span> 
+                                                                <a name="sponsor-channel-0" data-test-hint="StreamView-Link-2" href="/channels/walmart">
+                                                                    Walmart
+                                                                </a>
+                                                            </span>
+                                                        </div>
+                                                        <div class="_2v1D9">
+                                                            BeautySpaceNK Gifting Picks
+                                                        </div>
+                                                        <div class="_1e3VX">by 
+                                                            <a name="visit-channel-2" data-abbey="SELECT_VIEW_CHANNEL_HEADER" data-test-hint="StreamView-Link-3" href="/channels/beautyspacenk">BeautySpaceNK</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="_3zgH4">
+                                                        <div class="_3xeco">
+                                                            <a class="oH6sb _3k50D _1Vppk" data-abbey="SELECT_UNFOLLOW_CHANNEL_SHOW_HEADER" data-test-hint="FollowButton-Link-1">
+                                                                <span class="wo03T" data-abbey="SELECT_UNFOLLOW_CHANNEL_SHOW_HEADER">Following</span>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="streamViewFooter" class="wNT4g">
+                                        <div class="_27utF">
+                                            <div class="ceyTI">
+                                                <div class="_2ZoEg">
+                                                    <div class="_1N1Jk u2cLo">
+                                                        <button name="open-purchase-options-1" class="_1MqCv" data-abbey="VIEW_ALL_PRODUCTS" data-test-hint="StreamView-button-2" style="height:38px;">
+                                                            <span class="_3vhlc" data-abbey="VIEW_ALL_PRODUCTS">from</span>
+                                                            <span class="_6T0cp" data-abbey="VIEW_ALL_PRODUCTS">$9.00</span>
+                                                            <strong data-abbey="VIEW_ALL_PRODUCTS">BUY</strong>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div class="_2tIQ0">
                                         <div id="outer-container">
                                             <LiveMenu v-bind:class="[{showLeft: showLeft == 'true'},{offSlide: offSlide == 'true'}]"></LiveMenu>
